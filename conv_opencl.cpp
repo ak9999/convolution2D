83853056b5540cc5f335200c6d606ed07f9eebe0
Abdullah Khan <khanabdullahj@gmail.com>
2015-12-13 20:20:30 -0500

    Implemented convolution.cl and added OpenCL functions to conv_opencl.cpp.

--- conv_opencl.cpp
+++ conv_opencl.cpp
@@ -49,13 +49,92 @@ int main(int argc, char * argv[])
 
     int * out = new int[6*6]; // Initialize.
 
-    for(int i = 0; i < 6; i++) // Fill with random values.
+    for(int i = 0; i < 6; i++) // Fill with 0.
     {
         for(int j = 0; j < 6; j++)
             out[(i * 6) + j] = 0;
     }
 
-    convolute(matrix, 8, k_array, 3, out);
+    // Convolution happens here.
+
+    // Set up OpenCL environment.
+    cl_context context = 0;
+    cl_command_queue commandQueue = 0;
+    cl_program program = 0;
+    cl_device_id device = 0;
+    cl_kernel kernel = 0;
+    cl_mem memObjects[3] = {0,0,0};
+    cl_int errNum;
+
+    //Create an OpenCL context on first available platform
+    context = CreateContext();
+    if(context == NULL)
+    {
+        cerr << "Failed to create OpenCL context." << endl;
+        return 1;
+    }
+
+    //Create a command queue on first available device
+    commandQueue = CreateCommandQueue(context, &device);
+    if(commandQueue == NULL)
+    {
+        Cleanup(context, commandQueue, program, kernel, memObjects);
+        return 1;
+    }
+
+    //Create program from "HelloWorld.cl" kernel source
+    program = CreateProgram(context, device, "convolution.cl");
+    if(program == NULL)
+    {
+        Cleanup(context, commandQueue, program, kernel, memObjects);
+        return 1;
+    }
+
+    //Create OpenCL kernel
+    kernel = clCreateKernel(program, "convolution_kernel", NULL);
+    if(kernel == NULL)
+    {
+        cerr << "Failed to create kernel." << endl;
+        Cleanup(context, commandQueue, program, kernel, memObjects);
+        return true;
+    }
+
+    if(CreateMemObjects(context, memObjects, matrix, k_array) == false)
+	{
+		Cleanup(context, commandQueue, program, kernel, memObjects);
+		return 1;
+	}
+
+	//Set the kernel arguments
+	errNum = clSetKernelArg(kernel, 0 , sizeof(cl_mem), &memObjects[0]);
+	errNum = clSetKernelArg(kernel, 1 , sizeof(cl_mem), &memObjects[1]);
+	errNum = clSetKernelArg(kernel, 2 , sizeof(cl_mem), &memObjects[2]);
+	if(errNum != CL_SUCCESS)
+	{
+		cerr << "Error setting kernel arguments." << endl;
+		Cleanup(context, commandQueue, program, kernel, memObjects);
+		return 1;
+	}
+	size_t globalWorkSize[1] = {36};
+	size_t localWorkSize[1] = {1};
+
+	//Queue the kernel up for execution across the array
+	errNum = clEnqueueNDRangeKernel(commandQueue, kernel, 1, NULL, globalWorkSize, localWorkSize, 0, NULL, NULL);
+	if(errNum != CL_SUCCESS)
+	{
+		cerr << "Error ." << endl;
+		Cleanup(context, commandQueue, program, kernel, memObjects);
+		return 1;
+	}
+
+	//Transfer the result from buffer back to host
+	errNum = clEnqueueReadBuffer(commandQueue, memObjects[2], CL_TRUE, 0, sizeof(int)*36, out, 0, NULL, NULL);
+	if(errNum != CL_SUCCESS)
+	{
+		cerr << "Error reading result buffer." << endl;
+		Cleanup(context, commandQueue, program, kernel, memObjects);
+		return 1;
+	}
 
     for(int i = 0; i < 6; i++)
     {
@@ -65,6 +144,8 @@ int main(int argc, char * argv[])
         }
         cout << endl;
     }
+
+    Cleanup(context, commandQueue, program, kernel, memObjects);
 
     delete[] k_array;
     delete[] matrix;
--- convolution.cl
+++ convolution.cl
@@ -5,5 +5,29 @@ __kernel void convolution_kernel(__global const int *k_array,
                                  __global const int *input,
                                  __global int *output)
 {
-    
+    // 1D convolution requires that we use offsets.
+    int offset = get_global_id(0);
+    int sum = 0;
+    int row = (offset / 6);
+    offset = ((row * 8) + (offset % 6));
+
+    // Iterate KERNEL_SIZE times (9 times).
+    sum += (k_array[0] * input[offset]);
+    sum += (k_array[1] * input[++offset]);
+    sum += (k_array[2] * input[++offset]);
+    // After the loop iterates 3 times we must enter the next row.
+    // Increment offset by 6 to enter next row.
+    offset += 6;
+
+    sum += (k_array[3] * input[offset]);
+    sum += (k_array[4] * input[++offset]);
+    sum += (k_array[5] * input[++offset]);
+    offset += 6;
+
+    sum += (k_array[6] * input[offset]);
+    sum += (k_array[7] * input[++offset]);
+    sum += (k_array[8] * input[++offset]);
+
+    // Set the output equal to the sum.
+    output[get_global_id(0)] = ((int)sum);
 }
