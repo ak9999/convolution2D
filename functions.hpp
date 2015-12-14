5eb5102826f783ee75329941a6be451afb57efeb
Abdullah Khan <khanabdullahj@gmail.com>
2015-12-13 20:19:05 -0500

    Fixed OpenCL functions.

--- functions.hpp
+++ functions.hpp
@@ -23,7 +23,11 @@ void convolute(int ** output, int ** input, int ** kernel);
 void convolute(const int * input, size_t M, const int * kernel, size_t K, int * r);
 
 // OpenCL functions
-
+cl_context CreateContext();
+cl_command_queue CreateCommandQueue(cl_context, cl_device_id *);
+cl_program CreateProgram(cl_context, cl_device_id, const char *);
+bool CreateMemObjects(cl_context, cl_mem CreateMemObjects[3], int *, int *);
+void Cleanup(cl_context context, cl_command_queue commandQueue, cl_program program, cl_kernel kernel, cl_mem memObjects[3]);
 
 #include "functions.cpp"
 
